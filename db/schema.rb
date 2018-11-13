76479ff7721f62bea9e091ac271d65d9494864de
Ricardo Lima <ricardo@MacBook-Air-de-Ricardo.local>
2018-11-13 21:47:47 -0200

    migrate SQLITE to POSTGRES

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_12_204826) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "simulations", force: :cascade do |t|
     t.string "nome"
     t.string "email"
